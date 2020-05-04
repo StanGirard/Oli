34c9ba344ec18c6538f90f884f2535fb1a735aef
StanGirard <girard.stanislas@gmail.com>
2020-05-04 19:10:35 +0200

    [COMMAND] Create command folder

---
+++ oli/command/command.py
@@ -0,0 +1,44 @@
+import importlib
+def installation_finished():
+        input_user = str(input("Would you like to install something else ? Default yes (y/n) ") or "y")
+        if input_user.lower().startswith('y'):
+            return 0
+        else:
+            return 1
+            
+
+
+def run_input(switch, value):
+    string_value = switch.get(value, "Invalid Value")
+    if string_value != 'Invalid Value':
+        mymodule = importlib.import_module("." + string_value,  package=__package__)
+        return_code = mymodule.run()
+        if return_code is 0:
+            print("Installation Successfull")
+            
+        else:
+            print("An error occured during the installation")
+            print("Please check the logs to see what was the cause and run the command again")
+        return installation_finished()
+    print("Invalid Value")
+    return 0
+
+
+def check_input():
+    """Checks the type of the input and keeps asking if not an int """
+    
+    value = input("What would you like to install ? ")
+    while not value.isdigit():
+        value = input("What would you like to install ? (A number please) ")
+    return int(value)
+
+    
+
+def print_choice(switcher_value):
+    """Prints all the available choices"""
+    print("----------------------")
+    
+    for key in switcher_value:
+        print(str(key) + " - " + str(switcher_value[key]))
+
+    print("----------------------")
---
+++ oli/macos/run_process.py
@@ -0,0 +1,20 @@
+import subprocess
+def run_process(command_execute):
+    process = subprocess.Popen(command_execute, 
+                           stdout=subprocess.PIPE,
+                           shell=True,
+                           universal_newlines=True)
+    return_code_script = None
+    
+    while True:
+        output = process.stdout.readline()
+        print(output.strip())
+        # Do something else
+        return_code = process.poll()
+        if return_code is not None:
+            return_code_script = return_code
+            # Process has finished, read rest of the output 
+            for output in process.stdout.readlines():
+                print(output.strip())
+            break
+    return return_code_script
