17816e37f046512fd6b865aa9b614c6c88a1bb72
Stan <stan@pop-os.localdomain>
2020-05-06 16:10:38 +0200

    Netdata

--- oli/lib/linux_process.py
+++ oli/lib/linux_process.py
@@ -1,26 +1,22 @@
 import subprocess
 from oli.lib.command import bcolors
-
+import sys
 
 def run_process(command_execute):
     process = subprocess.Popen(command_execute, 
                            stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
                            shell=True,
                            universal_newlines=True)
-    return_code_script = None
     
     while True:
-        output = process.stdout.readline()
-        print(output.strip())
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            return_code_script = return_code
-            # Process has finished, read rest of the output 
-            for output in process.stdout.readlines():
-                print(bcolors.WARNING + output.strip() + bcolors.ENDC)
+        out = process.stdout.read(1)
+        if out == '' and process.poll() != None:
             break
-    return return_code_script
+        if out != '':
+            sys.stdout.write(out)
+            sys.stdout.flush()
+    return process.poll()
 
 
 def run_process_no_log(command_execute):
