0cea1f0b786513a4cdc656586c96515b9ec0e662
StanGirard <girard.stanislas@gmail.com>
2020-05-06 14:28:51 +0200

    [NETDATA] Linux

--- oli/linux/Netdata.py
+++ oli/linux/Netdata.py
@@ -1,7 +1,7 @@
 from oli.lib.linux_process import run_process, run_process_no_log
 
 def run():
-    if run_process(["bash <(curl -Ss https://my-netdata.io/kickstart.sh"]) == 0:
+    if run_process(['sh -c "$(curl -Ss https://my-netdata.io/kickstart.sh)"']) == 0:
         print("You can now access Netdata at http://localhost:19999")
         return 0
     else:
